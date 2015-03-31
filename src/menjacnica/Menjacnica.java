48f5ff666c847081121a2a92e39242d932cc787a
Home <Home@Home-PC>
2015-03-31 22:16:52 +0200

    Klasa Menjacnica je implementirala interface ali je ostala nefunkcionalna

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -1,10 +1,12 @@
 package menjacnica;
 
+import java.util.GregorianCalendar;
 import java.util.LinkedList;
 
+import menjacnica.interfejs.MenjacnicaInterface;
 import valute.Valuta;
 
-public class Menjacnica {
+public class Menjacnica implements MenjacnicaInterface {
 	private LinkedList<Valuta>valute=new LinkedList<Valuta>();
 
 	public LinkedList<Valuta> getValute() {
@@ -14,5 +16,8 @@ public class Menjacnica {
 	public void setValute(Valuta valuta) {
 		valute.add(valuta);
 	}
+	public void dodajKursValute(GregorianCalendar datum,String nazivValute,int prodajni, int kupovni,int srednji){}
+	public void obrisiKursValute(GregorianCalendar datum, String nazivValute){}
+	public Valuta pronadjiKurs(String nazivValute, GregorianCalendar datum){}
 
 }
