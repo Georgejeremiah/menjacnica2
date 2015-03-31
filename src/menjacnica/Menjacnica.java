ce5bde889132a414d7b3204d21d6bee4d84ae9a7
Home <Home@Home-PC>
2015-03-31 22:30:08 +0200

    U potpunosti implementiran interface MenjacnicaInterface

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -16,8 +16,31 @@ public class Menjacnica implements MenjacnicaInterface {
 	public void setValute(Valuta valuta) {
 		valute.add(valuta);
 	}
-	public void dodajKursValute(GregorianCalendar datum,String nazivValute,int prodajni, int kupovni,int srednji){}
-	public void obrisiKursValute(GregorianCalendar datum, String nazivValute){}
-	public Valuta pronadjiKurs(String nazivValute, GregorianCalendar datum){}
+	public void dodajKursValute(GregorianCalendar datum,String nazivValute,int prodajni, int kupovni,int srednji){
+		for(int i=0;i<valute.size();i++){
+			if(valute.get(i).getDatumValute().equals(datum)&&valute.get(i).getNaziv().equals(nazivValute)){
+				valute.get(i).setKupovniKurs(kupovni);
+				valute.get(i).setProdajniKurs(prodajni);
+				valute.get(i).setSrednjiKurs(srednji);
+			}
+		}
+	}
+	public void obrisiKursValute(GregorianCalendar datum, String nazivValute){
+		for(int i=0;i<valute.size();i++){
+			if(valute.get(i).getDatumValute().equals(datum)&&valute.get(i).getNaziv().equals(nazivValute)){
+				valute.get(i).setKupovniKurs(0);
+				valute.get(i).setProdajniKurs(0);
+				valute.get(i).setSrednjiKurs(0);}
+			}
+	}
+	public Valuta pronadjiKurs(String nazivValute, GregorianCalendar datum){
+		for(int i=0;i<valute.size();i++){
+			if(valute.get(i).getDatumValute().equals(datum)&&valute.get(i).getNaziv().equals(nazivValute)){
+			return valute.get(i);}
+			}
+		return null;
+		
+		
+	}
 
 }
